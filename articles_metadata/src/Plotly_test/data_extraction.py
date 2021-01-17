e7fcadb19b116c461387ad4cc36ccb0476d95790
Francesca Mangialardo <mangiafrangette@gmail.com>
2021-01-18 00:07:52 +0100

    dates fix

--- articles_metadata/src/Plotly_test/data_extraction.py
+++ articles_metadata/src/Plotly_test/data_extraction.py
@@ -1,88 +1,62 @@
-from urllib.request import urlopen
-import json
 import pandas as pd
-import plotly.express as px
-import os
-import time
-import geopandas as gpd
-import matplotlib.pyplot as plt
-import pycountry_convert
-import math
 import df_manipulation
 
 
-def calculate_num_of_articles_per_country(articles_df):
-    articles_per_country_dict = dict()
-    for index, article in articles_df.iterrows():
-        if article['countries'] is not None:
-            for country in article['countries']:
-                if country not in articles_per_country_dict:
-                    articles_per_country_dict[country] = 1
-                else:
-                    articles_per_country_dict[country] += 1
-    return articles_per_country_dict
-
-
-# mean_countries_dict in output: {'<country_code1>' : mean_abs_len1, ...}
-def mean_abstract_length(articles_df):
-    articles_df['abs_len'] = [len(abstract) for abstract in articles_df["abstract"].values]
-    articles_per_country_dict = calculate_num_of_articles_per_country(articles_df)
-    mean_countries_dict = dict()
-    for index, article in articles_df.iterrows():
-        if article['countries'] is not None:
-            for country in article['countries']:
-                if country not in mean_countries_dict:
-                    mean_countries_dict[country] = articles_df['abs_len'][index]*1.0/articles_per_country_dict[country]
-                else:
-                    mean_countries_dict[country] += articles_df['abs_len'][index]*1.0/articles_per_country_dict[country]
-    return pd.DataFrame({'iso_a3': mean_countries_dict.keys(), 'mean number of character per abstract': mean_countries_dict.values()}).set_index('iso_a3')
-
-
-# def productivity_country_per_topic(df_affiliations, df_topics, normalization="itra_country"):
-#     # precomputation:
-#     num_affs_series = df_affiliations.reset_index().groupby('article').size()#['affiliation']
-#     df_affs_topics = pd.DataFrame({'article': num_affs_series.index, 'num affs': num_affs_series.values}).set_index('article').merge(df_topics, on='article').merge(df_affiliations['year'], on='article')
-#
-#     df_articles_topics_distributed = df_affs_topics[df_affs_topics.columns.difference(['num affs', 'year'])].div(df_affs_topics.iloc[:, 0], axis='rows')
-#     df_affs_topics_distributed = df_affiliations.reset_index().set_index('article').merge(df_articles_topics_distributed, on='article')
-#     df_country_topics_absolut_productivity = df_affs_topics_distributed.groupby(['iso_a3', 'year']).sum()
-#     if normalization == "intra_country":
-#         df_country_topics_relative_productivity = df_country_topics_absolut_productivity.div(
-#             df_country_topics_absolut_productivity.sum(axis='columns'), axis='rows')
-#     elif normalization == "global":
-#         df_country_topics_relative_productivity = df_country_topics_absolut_productivity.div(
-#             df_country_topics_absolut_productivity.groupby('year').sum(), axis='rows')
-#     else:
-#         raise ValueError("normalization can be set only to 'intra_country' or 'global'")
-#     return df_country_topics_relative_productivity
-
-
-def productivity_country_per_topic(df_affiliations, df_topics, normalization='local'):
+def productivity_country_per_topic(df_affiliations, df_topics, normalization='both'):
     # precomputation:
-    num_affs_series = df_affiliations.reset_index().groupby('article').size()#['affiliation']
-    df_affs_topics = pd.DataFrame({'article': num_affs_series.index, 'num affs': num_affs_series.values}).set_index('article').merge(df_topics, on='article').merge(df_affiliations['year'], on='article')
+    num_affs_series = df_affiliations.reset_index().groupby('article').size()  # ['affiliation']
+    df_affs_topics = pd.DataFrame({'article': num_affs_series.index, 'num affs': num_affs_series.values}).set_index(
+        'article').merge(df_topics, on='article').merge(df_affiliations['year'], on='article')
 
-    df_articles_topics_distributed = df_affs_topics[df_affs_topics.columns.difference(['num affs', 'year'])].div(df_affs_topics.iloc[:, 0], axis='rows')
-    df_affs_topics_distributed = df_affiliations.reset_index().set_index('article').merge(df_articles_topics_distributed, on='article')
-    df_country_topics_absolut_productivity = df_affs_topics_distributed.groupby(['iso_a3', 'year']).sum()
-    if normalization == 'local':
-        df_country_topics_relative_productivity = df_country_topics_absolut_productivity.div(
-            df_country_topics_absolut_productivity.sum(axis='columns'), axis='rows')
+    df_articles_topics_distributed = df_affs_topics[df_affs_topics.columns.difference(['num affs', 'year'])].div(
+        df_affs_topics.iloc[:, 0], axis='rows')
+    df_articles_topics_distributed_single = df_articles_topics_distributed.reset_index().groupby('article').mean()
+    df_affs_topics_distributed = df_affiliations.reset_index().set_index('article').merge(
+        df_articles_topics_distributed_single, on='article')
+    df_country_topics_absolute_productivity = df_affs_topics_distributed.groupby(['iso_a3', 'year']).sum()
+    if normalization == 'None':
+        df_country_topics_relative_productivity = df_country_topics_absolute_productivity.groupby('year').sum()
         df_country_topics_relative_productivity['normalization'] = normalization
+        return df_country_topics_relative_productivity
+    elif normalization == 'absolute_year':
+        df_year_topics_absolute_productivity = df_country_topics_absolute_productivity.groupby('year').sum()
+        df_country_topics_relative_productivity = df_year_topics_absolute_productivity.div(
+            df_year_topics_absolute_productivity.sum(axis='columns'),
+            axis='rows')
+        df_country_topics_relative_productivity['normalization'] = normalization
+        return df_country_topics_relative_productivity
+    elif normalization == 'country_indipendent':
+        df_productivity_None = df_country_topics_absolute_productivity.groupby('year').sum()
+        df_productivity_None['normalization'] = 'None'
+        df_year_topics_absolute_productivity = df_country_topics_absolute_productivity.groupby('year').sum()
+        df_productivity_year = df_year_topics_absolute_productivity.div(
+            df_year_topics_absolute_productivity.sum(axis='columns'),
+            axis='rows')
+        df_productivity_year['normalization'] = 'absolute_year'
+        return pd.concat([df_productivity_None, df_productivity_year])
+    elif normalization == 'local':
+        df_country_topics_relative_productivity = df_country_topics_absolute_productivity.div(
+            df_country_topics_absolute_productivity.sum(axis='columns'), axis='rows')
+        df_country_topics_relative_productivity['normalization'] = normalization
+        return df_country_topics_relative_productivity
     elif normalization == 'global':
-        df_country_topics_relative_productivity = df_country_topics_absolut_productivity.div(
-            df_country_topics_absolut_productivity.groupby('year').sum(), axis='rows')
+        df_country_topics_relative_productivity = df_country_topics_absolute_productivity.div(
+            df_country_topics_absolute_productivity.groupby('year').sum(), axis='rows')
         df_country_topics_relative_productivity['normalization'] = normalization
+        return df_country_topics_relative_productivity
     elif normalization == 'both':
-        df_productivity_global = productivity_country_per_topic(df_affiliations, df_topics, normalization='global')
-        df_productivity_local = productivity_country_per_topic(df_affiliations, df_topics, normalization='local')
-        df_country_topics_relative_productivity = pd.concat([df_productivity_global, df_productivity_local])
+        df_productivity_global = df_country_topics_absolute_productivity.div(
+            df_country_topics_absolute_productivity.sum(axis='columns'), axis='rows')
+        df_productivity_global['normalization'] = 'global'
+        df_productivity_local = df_country_topics_absolute_productivity.div(
+            df_country_topics_absolute_productivity.sum(axis='columns'), axis='rows')
+        df_productivity_local['normalization'] = 'local'
+        return pd.concat([df_productivity_global, df_productivity_local])
     else:
-        raise ValueError("normalization can be set only to 'local', 'global' or 'both'")
-    return df_country_topics_relative_productivity
+        raise ValueError("normalization can be set only to 'local', 'global', 'both', 'None'")
 
 
-def random_tests():
+def main():
     path_of_files = "../../data/research_papers/one_folder_metadata"
     path_of_topics_csv = "doc_topics.csv"
 
@@ -109,10 +83,9 @@ def random_tests():
     # df_tot = pd.concat([df_prod_glob, df_prod_loc])#, keys=['global', 'local'])
     # print(df_tot)
     # print(df_prod_glob.merge(df_prod_loc, on='normalization'))
-    df_tot = productivity_country_per_topic(df_aff, df_topics, normalization='both')
+    df_tot = productivity_country_per_topic(df_aff, df_topics, normalization='country_indipendent')
     print(df_tot.query("iso_a3 == 'USA'"))
 
 
-
 if __name__ == '__main__':
-    random_tests()
+    main()
--- articles_metadata/src/Plotly_test/df_manipulation.py
+++ articles_metadata/src/Plotly_test/df_manipulation.py
@@ -1,13 +1,9 @@
-from urllib.request import urlopen
 import json
 import pandas as pd
-import plotly.express as px
 import os
-import time
 import geopandas as gpd
-import matplotlib.pyplot as plt
-import pycountry_convert
-import math
+from pycountry_convert import country_alpha3_to_country_alpha2, country_alpha2_to_continent_code, \
+    country_name_to_country_alpha3, map_country_alpha3_to_country_name
 import copy
 import data_extraction
 
@@ -28,6 +24,7 @@ def create_articles_dfs(path_of_files):
 def new_create_articles_dfs(path_of_files):
     list_of_articles = []
     folder = os.fsencode(path_of_files)
+    count = 0
     dict_of_journals = dict()
     affiliations_list = []
     for file in os.listdir(folder):
@@ -37,65 +34,46 @@ def new_create_articles_dfs(path_of_files):
                 article_dict = json.load(article_file)
                 authors = article_dict.pop('authors')
                 for author in authors:
-                    # curr_author_affs = []
-                    for affiliation in author['affiliation']:
-                        curr_aff = {'journal': article_dict['journal_title'],
-                                    'article': os.path.splitext(filename)[0],
-                                    'author': f"{author['given']} {author['family']}",
-                                    'affiliation': affiliation['original_name'],
-                                    'original_name': affiliation['original_name'],
-                                    'year': article_dict['date'],
-                                    'country': None,
-                                    'iso_a3': None
-                                    }
-                        if affiliation['normalized_name'] is not None:
-                            curr_aff['affiliation'] = affiliation['normalized_name']
-                            curr_aff['country'] = affiliation['country']
-                            curr_aff['iso_a3'] = pycountry_convert.country_name_to_country_alpha3(
-                                affiliation['country'])
-                        affiliations_list.append(curr_aff)
-                list_of_articles.append(article_dict)
+                    if author['affiliation'] is not None:
+                        for affiliation in author['affiliation']:
+                            if affiliation is not None:
+                                if affiliation['original_name'] is not None:
+                                    curr_aff = {'journal': article_dict['journal_title'],
+                                                'article': os.path.splitext(filename)[0],
+                                                'author': f"{author['given']} {author['family']}",
+                                                'affiliation': affiliation['original_name'],
+                                                'original_name': affiliation['original_name'],
+                                                'year': article_dict['date'],
+                                                'country': None,
+                                                'iso_a3': None
+                                                }
+                                    if affiliation['normalized_name'] is not None:
+                                        curr_aff['affiliation'] = affiliation['normalized_name']
+                                        curr_aff['country'] = affiliation['country']
+                                        curr_aff['iso_a3'] = country_name_to_country_alpha3(
+                                            affiliation['country'])
+                                    affiliations_list.append(curr_aff)
+                                else:
+                                    count += 1
+                                    # print("aff [{None}]")
+                                    # print(count)
+                                    # print(author)
+                                    # print(article_dict['article_title'])
+                            else:
+                                count += 1
+                                # print("aff [None]")
+                                # print(count)
+                                # print(author)
+                                # print(article_dict['article_title'])
+                    else:
+                        count += 1
+                        # print("aff None")
+                        # print(count)
+                        # print(author)
+                        # print(article_dict['article_title'])
+            list_of_articles.append(article_dict)
     return pd.DataFrame(list_of_articles), pd.DataFrame(affiliations_list).set_index(
         ['journal', 'article', 'author', 'affiliation']).sort_index()
-
-
-def articles_dfs_DataSudio(path_of_files):
-    list_of_articles = []
-    folder = os.fsencode(path_of_files)
-    dict_of_journals = dict()
-    affiliations_list = []
-    for file in os.listdir(folder):
-        filename = os.fsdecode(file)
-        if filename.endswith('.json'):
-            with open(f'{path_of_files}/{filename}', 'r', encoding='utf-8') as article_file:
-                article_dict = json.load(article_file)
-                # print(list(article_dict.keys()))
-                authors = article_dict.pop('authors')
-                for author in authors:
-                    # curr_author_affs = []
-                    for affiliation in author['affiliation']:
-                        curr_aff = {'url': article_dict['url'],
-                                    'publisher': None if 'publisher' not in article_dict.keys() else article_dict[
-                                        'publisher'],
-                                    'journal': article_dict['journal_title'],
-                                    'article_title': article_dict['article_title'],
-                                    'article': os.path.splitext(filename)[0],
-                                    'author': f"{author['given']} {author['family']}",
-                                    'affiliation': affiliation['original_name'],
-                                    'original_name': affiliation['original_name'],
-                                    'year': article_dict['date'],
-                                    'country': None,
-                                    'iso_a3': None
-                                    }
-                        if affiliation['normalized_name'] is not None:
-                            curr_aff['affiliation'] = affiliation['normalized_name']
-                            curr_aff['country'] = affiliation['country']
-                            curr_aff['iso_a3'] = pycountry_convert.country_name_to_country_alpha3(
-                                affiliation['country'])
-                        affiliations_list.append(curr_aff)
-                list_of_articles.append(article_dict)
-    return pd.DataFrame(list_of_articles), pd.DataFrame(affiliations_list).set_index(
-        ['publisher', 'journal', 'article', 'author', 'affiliation']).sort_index()
 
 
 def create_topics_df(path_of_topics_csv):
@@ -107,8 +85,12 @@ def create_topics_df(path_of_topics_csv):
 
 def load_world_df():
     world_df = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-    world_df = world_df[(world_df.pop_est > 0) & (world_df.name != "Antarctica")]
-    return world_df.set_index('iso_a3')
+    countries_to_modify = ['France', 'Norway', 'Somaliland']
+    for country in countries_to_modify:
+        world_df.loc[world_df['name'] == country, 'iso_a3'] = country_name_to_country_alpha3(country)
+    # world_df = world_df[(world_df.pop_est > 0) & (world_df.name != "Antarctica")]
+    return world_df.set_index('iso_a3').drop(columns=['gdp_md_est', 'geometry', 'pop_est']).rename(
+        columns={'name': 'country'})
 
 
 # aggregation_dict = {"hypertopic 1": ["2", "3", "5"], "hypertopic 2": ["7", "8"], ... }
@@ -129,12 +111,35 @@ def generate_bubble_chart_column(world_df, topics_to_show):
 
 
 def add_data_to_world_df(world_df, data_df):
+    missing_countries = list(set(data_df.reset_index()['iso_a3']).difference(set(world_df.index)))
+    df_missing_countries = pd.DataFrame(
+        [[alpha3_to_continent(iso_a3), alpha3_to_country(iso_a3)] for iso_a3 in missing_countries],
+        columns=world_df.columns.values.tolist(), index=missing_countries)
+    df_missing_countries.index.name = 'iso_a3'
+    world_df = world_df.append(df_missing_countries)
     return world_df.merge(data_df, on='iso_a3')
 
 
+def alpha3_to_country(iso_alpha3):
+    return map_country_alpha3_to_country_name()[iso_alpha3]
+
+
+def alpha3_to_continent(iso_alpha3):
+    return {
+        'EU': 'Europa',
+        'NA': 'North America',
+        'SA': 'South America',
+        'AS': 'Asia',
+        'OC': 'Oceania',
+        'AF': 'Africa',
+    }[country_alpha2_to_continent_code(country_alpha3_to_country_alpha2(iso_alpha3))]
+
+
 def magic_function_that_does_everything_ma_non_il_caffe(path_of_json_files, path_of_topic_modeling_csv,
-                                                        path_of_csv_for_DataStudio, hypertopics_aggregation_dict):
+                                                        hypertopics_aggregation_dict,
+                                                        source_tag):
     df_articles, df_affiliations = new_create_articles_dfs(path_of_json_files)
+    df_affiliations['source_tag'] = source_tag
     df_topics = create_topics_df(path_of_topic_modeling_csv)
 
     hypertopics_list = list(hypertopics_aggregation_dict.keys())
@@ -143,53 +148,83 @@ def magic_function_that_does_everything_ma_non_il_caffe(path_of_json_files, path
     productivity = data_extraction.productivity_country_per_topic(df_affiliations, df_hypertopics, normalization='both')
     productivity = productivity.reset_index().set_index('iso_a3')
 
+    productivity_country_indipendent = data_extraction.productivity_country_per_topic(df_affiliations, df_hypertopics,
+                                                                                      normalization='country_indipendent')
+
     world_df = load_world_df()
-    world_df = add_data_to_world_df(world_df, productivity).sort_values(by=['year'])
-    wolrd_df_DataStudio = world_df.drop(columns=['gdp_md_est', 'geometry', 'pop_est']).rename(
-        columns={'name': 'country'})
+    wolrd_df_DataStudio = add_data_to_world_df(world_df, productivity).sort_values(by=['year'])
 
     topics_to_show = hypertopics_list
     bubble_df = generate_bubble_chart_column(wolrd_df_DataStudio, topics_to_show)
-    bubble_df.to_csv(path_of_csv_for_DataStudio, index=True, header=True)
-    print("I have finished to write the csv file! :) You can upload it to DataStudio")
+    bubble_df['source_tag'] = source_tag
+
+    df_no_country_productivity = generate_bubble_chart_column(productivity_country_indipendent, topics_to_show)
+    df_no_country_productivity['source_tag'] = source_tag
+
+    return bubble_df, df_affiliations, df_no_country_productivity
+    # bubble_df.to_csv(path_of_csv_for_DataStudio, index=True, header=True)
+    # print("I have finished to write the csv file! :) You can upload it to DataStudio")
 
 
 def main():
     # INPUTS
     path_of_json_research_papers = "../../data/research_papers/one_folder_metadata"
-    path_of_json_conference_papers = "../../ILPERCORSOCHEVUOIPORCODIO"
-    path_of_topic_modeling_csv = "doc_topics.csv"
-    path_of_csv_for_DataStudio_research_papers = "research_wolrd_df_DataStudio_bubble.csv"
-    path_of_csv_for_DataStudio_conference_papers = "conference_wolrd_df_DataStudio_bubble.csv"
-    hypertopics_aggregation_dict_research = {
-        "Text analysis/mining": ["1", "3", "4", "9", "11", "12"],
-        "Digital heritage and multimedia": ["2", "16"],
-        "Textual scholarship": ["5"],
-        "Data preservation and  digital curation": ["7", "8", "14"],
-        "DH discipline": ["13"],
-        "NLP": ["10"],
-        "Other": ["6"],
-        "Other/Hypertext": ["15"]
-    }
-    hypertopics_aggregation_dict_conference = {
-        "Text analysis and mining": ["8"],
-        "Literary text analysis": ["1", "6"],
-        "Textual scholarship": ["7", "9"],
-        "Data preservation and curation": ["4"],
-        "DH discipline": ["2", "11"],
-        "Digital history": ["3"],
-        "Digital libraries": ["10"],
-        "Other/Measuring": ["5"],
-        "Other": ["12"]
+    path_of_json_conference_papers = "../../data/adho_conferences/one_folder_metadata"
+    path_of_topic_modeling_csv_research = "../../data/research_papers/research_bow_lemmatized_16topics/doc_topics.csv"
+    path_of_topic_modeling_csv_conference = "../../data/adho_conferences/conference_bow_12_lemmatized/doc_topics.csv"
+
+    path_of_csv_for_datastudio = "complete_df_DataStudio.csv"
+
+    inputs_dict = {
+        "journal": {
+            "path_of_json_papers": path_of_json_research_papers,
+            "path_of_topic_modeling_csv": path_of_topic_modeling_csv_research,
+            "hypertopics_aggregation": {
+                "Text analysis/mining": ["1", "3", "4", "9", "11", "12"],
+                "Digital heritage and multimedia": ["2", "16"],
+                "Textual scholarship": ["5"],
+                "Data preservation and  digital curation": ["7", "8", "14"],
+                "DH discipline": ["13"],
+                "NLP": ["10"],
+                "Other": ["6", "15"]
+            }
+        },
+        "conference": {
+            "path_of_json_papers": path_of_json_conference_papers,
+            "path_of_topic_modeling_csv": path_of_topic_modeling_csv_conference,
+            "hypertopics_aggregation": {
+                "Text analysis and mining": ["8"],
+                "Literary text analysis": ["1", "6"],
+                "Textual scholarship": ["7", "9"],
+                "Data preservation and curation": ["4"],
+                "DH discipline": ["2", "11"],
+                "Digital history": ["3"],
+                "Digital libraries": ["10"],
+                "Other": ["5", "12"]
+            }
+        }
     }
 
     # OUTPUT: IT WRITES THE CSV TO UPLOAD IN DATASTUDIO
-    magic_function_that_does_everything_ma_non_il_caffe(path_of_json_research_papers, path_of_topic_modeling_csv,
-                                                        path_of_csv_for_DataStudio_research_papers,
-                                                        hypertopics_aggregation_dict_research)
-    magic_function_that_does_everything_ma_non_il_caffe(path_of_json_conference_papers, path_of_topic_modeling_csv,
-                                                        path_of_csv_for_DataStudio_conference_papers,
-                                                        hypertopics_aggregation_dict_conference)
+    world_dfs_list_for_datastudio = []
+    aff_dfs_list_for_datastudio = []
+    dfs_no_country_productivity = []
+    for source_tag in inputs_dict.keys():
+        world_df, df_affs, df_no_country_productivity = magic_function_that_does_everything_ma_non_il_caffe(
+            inputs_dict[source_tag]['path_of_json_papers'],
+            inputs_dict[source_tag]['path_of_topic_modeling_csv'],
+            inputs_dict[source_tag]['hypertopics_aggregation'],
+            source_tag)
+        world_dfs_list_for_datastudio.append(world_df)
+        aff_dfs_list_for_datastudio.append(df_affs)
+        dfs_no_country_productivity.append(df_no_country_productivity)
+
+    world_df_for_datastudio = pd.concat(world_dfs_list_for_datastudio)
+    world_df_for_datastudio.to_csv("complete_df_DataStudio.csv", index=True, header=True)
+    aff_df_for_datastudio = pd.concat(aff_dfs_list_for_datastudio)
+    aff_df_for_datastudio.to_csv("affiliation_df_DataStudio.csv", index=True, header=True)
+    no_country_productivity_datastudio = pd.concat(dfs_no_country_productivity)
+    no_country_productivity_datastudio.to_csv("no_country_productivity_DataStudio.csv", index=True, header=True)
 
 
 if __name__ == '__main__':
